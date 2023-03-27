e81e35402ae7c226e5e150bd73b80f3b5c0e4711
Matheus Felipe <fpmitmatheus@yahoo.com>
2023-03-26 19:36:35 -0500

    requisito 2 - teste

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui. Deus seja louvado sempre"""
-    raise NotImplementedError
+    # raise NotImplementedError
+    # 1 - pego o intervalo de tempo
+    # 2 - pego o primeiro horário do intervalo e passo por cada índice do array
+    # 3 - aquele índice que conter o horário, target_time, será contabilizado
+    # 4 - Coloco essa contalilidade num array
+    # 5 - retorno o maior contador
+    
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    # pass
+    # result1 = encrypt_message("teste", "1")
+    with pytest.raises(TypeError):
+        encrypt_message("teste", "1")
+
+    # result2 = encrypt_message(2, 1)
+    with pytest.raises(TypeError):
+        encrypt_message(2, 1)
+
+    assert encrypt_message("arara", 100) == "arara"
+
+    assert encrypt_message("matheus", 3) == "tam_sueh"
+
+    assert encrypt_message("matheus", 4) == "sue_htam"
