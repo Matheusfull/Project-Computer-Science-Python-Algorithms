aa57735a90fb2c5fcbfe08ed4489748cdff76e80
Matheus Felipe <fpmitmatheus@yahoo.com>
2023-03-28 14:45:05 -0500

    requisito 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,29 @@
 def is_palindrome_iterative(word):
     """Faça o código aqui"""
-    raise NotImplementedError
+    # raise NotImplementedError
+    if len(word) == 1:
+        return True
+
+    if word == '':
+        return False
+
+    mid = 0
+    first_half = ''
+    second_half = ''
+
+    if len(word) % 2 == 0:
+        mid = int((len(word)) / 2)
+        first_half = word[:mid]
+        second_half = word[mid:]
+    else:
+        mid = int((len(word) + 1) / 2)
+        first_half = word[:mid-1]
+        second_half = word[mid:]
+
+    if list(first_half) == list(reversed(second_half)):
+        return True
+    else:
+        return False
+
+
+# print(is_palindrome_iterative("ESxSE"))
